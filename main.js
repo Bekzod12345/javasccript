5253b007574beaa577b077c8ed75d4e64d9f8d26
Bekzod12345 <127377732+Bekzod12345@users.noreply.github.com>
2023-07-06 16:01:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+
+
+// let text = "salom"
+// console.log(typeof text);
+// ////function
+
+// console.log(text.charAt(text.length-1));
+
+// console.log(text.endsWith("m "));
+// console.log(text.includes("m"));
+// ////soni tekshiradiu
+// console.log(text.indexOf("l"));
+
+// console.log(text.lastIndexOf("m"));
+// //// uzinlik tekshradi
+// console.log(text.length);
+
+// let end = text.padEnd(100,"s")
+// let start = text.padStart(100,"k")
+// console.log(end.concat("urtasi"));
+// console.log(text.repeat(5));
+// console.log(text.replace(text,.
+//     ));
+
+// let son = prompt("son")-0
+
+// for(let i = 1; i > 101; i++){
+//     if (i%15===0) {
+//         console.log('fizbuzz=${i}');
+        
+//     }
+//     else if (i%3===0) {
+//         console.log('fizz = $ {i}');
+        
+//     }
+//     else if (i%5===0) {
+//         console.log('buzz = $ {i}');
+        
+//     }
+//     else{
+//         console.log('musr=${i}');
+//     }
+
+// }
+
+
+// let string = "iqbol4185451"
+
+// let number = '';
+// for (let i = 0; i<string.length; i++){
+//     console.log(i,string[i])
+//     if (Number(string[i])) {
+//         number+=string[i]
+        
+//     }
+// }
+// console.log(number-0);
+
+// let km = "10km"
+// let result = ""
+// if (km[km.length-2]==="k") {
+//     for (let i = 0; i< km.length; i++){
+//         if(Number(km[i]) || Number(km[i])=== 0){
+//             result+=km[i]
+//         }
+//     } 
+//     result = ( result-0)*10
+//      console.log(result + "m");
+// }
+    
+
+
+let son = prompt("bilet narxi 5ming")
+if (son >= 5000) {
+    alert("chiptangiz tayor")
+}else{
+    alert("pulingiz yetmaydi")
+}
